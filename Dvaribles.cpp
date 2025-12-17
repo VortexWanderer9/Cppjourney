c7cf1ad5f56f892dbfb35df741e432b5436d6bcd
Tyrell Wellick <keshavniure99@gmail.com>
2025-12-16 22:04:34 -0900

    Add initial implementation of Dvaribles.cpp with basic variable declarations and output

---
+++ Dvaribles.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num = 23;
+    double noNum = 34.67;
+    string message = "Hello World";
+    char sym = 'M';
+   bool whoIam = false;
+    cout << num << "\n" << noNum << "\n" << message <<  "\n" << sym << "\n" <<  whoIam;
+return 0;
+}
