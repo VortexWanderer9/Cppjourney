643ae759c4cc0beffc1f59f0d206ced27192fed8
Tyrell Wellick <keshavniure99@gmail.com>
2025-12-16 22:13:52 -0900

    learn how to take input from user and process it.

---
+++ sumTwoNumb.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+int x, y;
+cout  << "Enter first Number:";
+cin >> x ;
+cout << "Enter Second Number :";
+cin >> y;
+int sum = x + y;
+cout << "Sum of " << x << " And " << y << " is " << sum;
+    return 0;
+}
---
+++ userInput.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int x;
+    cout << "Think a number :" << endl;
+    cin >> x;
+    cout << "Your number is:" << x ;
+    return 0;
+}
