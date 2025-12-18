bea18adaebdd68773351c031b5de711a188f5735
Tyrell Wellick <keshavniure99@gmail.com>
2025-12-17 21:40:02 -0900

    c++ data types

---
+++ dataTypes.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int number = 12;
+    char letter = 'A';
+    bool isTrue = false;
+    float score = 4.5;
+    string message = "Hello world !";
+    cout << "Number is :" << number << "\n"
+         << "letter is:" << letter << "\nWhy :" << isTrue << "\nmessage :" << message;
+    return 0;
+}
