2af6a9e312c864315d33c408185eb171a12157a4
Tyrell Wellick <keshavniure99@gmail.com>
2025-12-31 03:47:01 -0900

    Refactor Struct.cpp to add language struct and user input functionality for programming details

--- Struct.cpp
+++ Struct.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 struct Person
 {
@@ -6,12 +7,38 @@ struct Person
     int age;
 } student;
 
+struct language
+{
+    string programming;
+    int experience;
+    char isAvailable;
+    int age;
+};
+void make()
+{
+    language programmer1;
+    cout << "Enter Programming language that you use: ";
+    getline(cin, programmer1.programming);
+    cout << "Enter Your Experience in year: ";
+    cin >> programmer1.experience;
+    cout << "Are You Available ? (Y or N): ";
+    cin >> programmer1.isAvailable;
+    cout << "What is your age : ";
+    cin >> programmer1.age;
+    string work = (programmer1.isAvailable == 'Y') ? "You are available for work" : "You are not available for work";
+    cout << "------------You have just entered------------" << endl;
+    cout << " \nYour Name is" << programmer1.programming << "\n"
+         << "\nYou Have" << programmer1.experience << "yeas of experience" << "\n"
+         << "\n"
+         << work << "\nYou are" << programmer1.age << "Years Old";
+}
 int main()
 {
-    cout << "Enter Your Name: ";
-    getline(cin, student.name);
-    cout << "Enter Your Age: ";
-    cin >> student.age;
-    cout << "Your Name is " << student.name << " Your are " << student.age;
+    // cout << "Enter Your Name: ";
+    // getline(cin, student.name);
+    // cout << "Enter Your Age: ";
+    // cin >> student.age;
+    // cout << "Your Name is " << student.name << " Your are " << student.age;
+    make();
     return 0;
 }
